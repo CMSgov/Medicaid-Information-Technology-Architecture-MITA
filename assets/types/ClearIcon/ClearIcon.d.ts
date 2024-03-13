9e96b6d338c6947e13987c807784c9392afc2790
smahoney10 <smahoney10@gmail.com>
2024-03-13 12:05:40 -0400

    test remove folders

--- assets/types/ClearIcon/ClearIcon.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-/**
- * <ClearIcon> is an internal component used by <FilterChip>
- */
-export declare function ClearIcon(): React.ReactElement;
-export default ClearIcon;
--- assets/types/DateField/defaultDateFormatter.d.ts
+++
@@ -1,7 +0,0 @@
-export interface DateObject {
-    day: string;
-    month: string;
-    year: string;
-}
-export declare const defaultDateFormatter: (dateObject: DateObject) => DateObject;
-export default defaultDateFormatter;
