6074e441a1eaa3a2298e27372c31bffe0d53e303
GM921 <gmolla@mitre.org>
2025-08-05 15:10:11 -0400

    Add files via upload

---
+++ assets/js/toc-expand-collapse.js
@@ -0,0 +1,54 @@
+(function setupNavbarExpansion() {
+  const path = document.getElementById("toc-script").getAttribute("data-path");
+
+  // Load the toc expansion state from cookies, if they exist.
+  const cookie = (
+    document.cookie
+      .split(";")
+      .filter(function (c) {
+        return c.trim().substr(0, 12) === "expandState=";
+      })
+      .pop() || "={}"
+  ).split("=")[1];
+  const expandState = JSON.parse(decodeURIComponent(cookie));
+
+  function toggleNav(e) {
+    const button = e.target;
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
+    const controlledID = button.getAttribute("aria-controls");
+    const controlled = document.getElementById(controlledID);
+
+    controlled.style.display = isExpanded ? "none" : "";
+    button.setAttribute("aria-expanded", !isExpanded);
+
+    // Update the expand state with the new info, then store that in a cookie.
+    // Need to set the path on the cookie so that all pages within this site
+    // will use the same one; otherwise, the pages in subdirectories will
+    // control a seprate cookie. Also keep the cookie around for a year.
+    Object.assign(expandState, { [controlledID]: !isExpanded });
+    document.cookie = [
+      "expandState=",
+      encodeURIComponent(JSON.stringify(expandState)),
+      ";path=",
+      path,
+      ";max-age=",
+      365 * 24 * 60 * 60, // a year in seconds
+    ].join("");
+  }
+
+  Array.from(
+    document.querySelectorAll("button.ds-c-vertical-nav__label[aria-controls]")
+  ).forEach(function (button) {
+    button.addEventListener("click", toggleNav);
+
+    const controlledID = button.getAttribute("aria-controls");
+
+    // If the expand-controlled element exists in the known expansion state,
+    // update the DOM accordingly.
+    if (typeof expandState[controlledID] === "boolean") {
+      button.setAttribute("aria-expanded", expandState[controlledID]);
+      const controlled = document.getElementById(controlledID);
+      controlled.style.display = expandState[controlledID] ? "" : "none";
+    }
+  });
+})();
